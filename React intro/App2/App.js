501b470a329efe9f407ef79a9e9cc18fade09616
sarthakgadekar <sarthakgadekar360@gmail.com>
2024-04-12 19:52:38 +0530

    learned about parcel, npm registry, bundlers, package.json and lock file

--- React intro/App2/App.js
+++ React intro/App2/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom"
+
 const heading = React.createElement(
   "h1",
   { id: "heading" },
@@ -10,10 +13,16 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading); //this render method is converting object "Heading" into a h1 Tag and putting it into browser's DOM
 
+/* 
+// we will never use below way to create react elements. Instead we will use jsx
 const object = React.createElement("h1", { id: "heading-2" }, [
   React.createElement("h1", { id: "child-1" }, "Child-1"),
   React.createElement("h1", { id: "child-1" }, "Child-2")
 ]);
+ */
+
+const object= <h1>Hello React. This is jsx way of inserting element into dom</h1>
+
 
 const root2=ReactDOM.createRoot(document.getElementById("root"));
 root2.render(object);
